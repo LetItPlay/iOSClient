fd47c2317a144aa4c1b6d584215bd3b7fb2236c1
Ivan Gorbulin <ivan.gorbulin@gmail.com>
2017-08-30 13:09:50 +0500

    #3 tabbar with pictures, no disabled message

--- blockchainapp/AppDelegate.swift
+++ blockchainapp/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        setupAppearence()
+        
         return true
     }
 
@@ -41,6 +43,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    // MARK: - appearence setup
+    func setupAppearence() {
+        UITabBar.appearance().tintColor = UIColor.vaCharcoalGrey
+    }
 
 }
 
---
+++ blockchainapp/extension/StyleExtension.swift
@@ -0,0 +1,35 @@
+//
+//  StyleExtension.swift
+//  blockchainapp
+//
+//  Created by Ivan Gorbulin on 30/08/2017.
+//  Copyright © 2017 Ivan Gorbulin. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var vaBlack: UIColor {
+        return UIColor(white: 3.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var vaTomato: UIColor {
+        return UIColor(red: 243.0 / 255.0, green: 71.0 / 255.0, blue: 36.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var vaWhite: UIColor {
+        return UIColor(white: 250.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var vaCharcoalGrey: UIColor {
+        return UIColor(white: 74.0 / 255.0, alpha: 1.0)
+    }
+}
+
+// Sample text styles
+
+extension UIFont {
+    class func vaHeaderFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 24.0, weight: UIFontWeightRegular)
+    }
+}
