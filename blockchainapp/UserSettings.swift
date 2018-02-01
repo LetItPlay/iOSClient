f1d9cb17e22f11b240c7652721b58609af340e96
Polina Abrosimova <abrosimowa97@gmail.com>
2018-02-01 19:11:49 +0500

    Profile saves name and image from gallery and camera

--- blockchainapp/UserSettings.swift
+++ blockchainapp/UserSettings.swift
@@ -7,12 +7,57 @@
 //
 
 import Foundation
+import UIKit
 
 enum Language: String {
 	case en = "en", ru = "ru", none = "suicide silence"
 }
 
 class UserSettings {
+    static var name: String {
+        get {
+            let def = UserDefaults.standard
+            var res: String
+            if let str = def.value(forKey: "name") as? String {
+                res = str
+            } else {
+                def.setValue("name", forKey: "name")
+                def.synchronize()
+                res = "name"
+            }
+            return res
+        }
+        set(newName) {
+            if newName != ""
+            {
+                let def = UserDefaults.standard
+                def.setValue(newName, forKey: "name")
+                def.synchronize()
+            }
+        }
+    }
+    
+    static var image: Data {
+        get {
+            let def = UserDefaults.standard
+            var res: Data
+            if let data = def.value(forKey: "image") as? Data {
+                res = data
+            } else {
+                res = UIImagePNGRepresentation(UIImage.init(named: "placeholder")!)!
+                def.set(res, forKey: "image")
+                def.synchronize()
+            }
+            return res
+        }
+        
+        set(newImage) {
+            let def = UserDefaults.standard
+            def.setValue(newImage, forKey: "image")
+            def.synchronize()
+        }
+    }
+    
 	static var language: Language {
 		get {
 			let def = UserDefaults.standard
