78951aa65982e3e0aae3a9d3f2b110784cde041b
Polina <abrosimowa@maugry.ru>
2018-02-21 09:43:31 +0500

    Methods for server update manager

--- blockchainapp/RequestManager.swift
+++ blockchainapp/RequestManager.swift
@@ -22,6 +22,11 @@ enum TrackUpdateRequest {
     case report(msg: String)
 }
 
+enum ChannelUpdateRequest {
+    case subscribe(count: Int)
+    case report(msg: String)
+}
+
 fileprivate extension TracksRequest {
     func urlQuery(lang: String) -> String {
         switch self {
@@ -144,6 +149,68 @@ class RequestManager {
             })
         }
         return Observable.error(RequestError.invalidURL)
+    }
+    
+    func channelUpdate(id: Int, type: ChannelUpdateRequest) -> Observable<Station1> {
+        return Observable<Station1>.create({ (observer) -> Disposable in
+            if let str = String(format: "https://manage.letitplay.io/api/stations/%d/counts/", id).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+                let url = URL(string: str) {
+                
+                var request = URLRequest(url: url)
+                request.httpMethod = "POST"
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+                var elements: [String: Int] = [:]
+                elements["report_count"] = 0
+                elements["subscription_count"]   = 0
+                
+                switch type {
+                case .subscribe(let count):
+                    elements["subscription_count"] = count
+                case .report(let _):
+                    elements["report_count"] = 1
+                }
+                
+                request.httpBody = try? JSONSerialization.data(withJSONObject: elements, options: .prettyPrinted)
+                
+                Alamofire.request(url, method: .post, parameters: elements, encoding: URLEncoding.default, headers: nil)
+                    .responseData { (response: DataResponse<Data>) in
+                        
+                        if let _ = response.error {
+                            observer.onError(RequestError.noConnection)
+                            observer.onCompleted()
+                            return
+                        }
+                        
+                        if let resp = response.response, let data = response.data {
+                            if resp.statusCode == 200 {
+                                do {
+                                    let json  = try JSON(data: data)
+                                    if let channel = Station1.init(json: json) {
+                                        observer.onNext(channel)
+                                    } else {
+                                        observer.onError(RequestError.invalidJSON)
+                                    }
+                                } catch(let error) {
+                                    print(error)
+                                    observer.onError(RequestError.invalidJSON)
+                                }
+                            } else {
+                                observer.onError(RequestError.serverError(code: resp.statusCode, msg: String(data: data, encoding: .utf8) ?? ""))
+                            }
+                        } else {
+                            observer.onError(RequestError.noConnection)
+                        }
+                        observer.onCompleted()
+                }
+            } else {
+                observer.onError(RequestError.invalidURL)
+            }
+            
+            return Disposables.create {
+                print("Track update signal disposed")
+            }
+        })
     }
     
     func trackUpdate(id: Int, type: TrackUpdateRequest) -> Observable<Track1> {
--- blockchainapp/ServerUpdateManager.swift
+++ blockchainapp/ServerUpdateManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RxSwift
 
 enum TrackAction {
 	case like
@@ -23,12 +24,38 @@ enum StationAction {
 
 class ServerUpdateManager {
 	static let shared = ServerUpdateManager()
+    
+    let disposeBag = DisposeBag()
 	
 	func makeStation(id: Int, action: StationAction) {
-		
+        let type: ChannelUpdateRequest
+        switch action {
+        case .subscribe:
+            type = .subscribe(count: 1)
+        case .unsubscribe:
+            type = .subscribe(count: -1)
+        case .report(let msg):
+            type = .report(msg: msg)
+        }
+        RequestManager.shared.channelUpdate(id: id, type: type).subscribe(onNext: { (channel) in
+            NotificationCenter.default.post(name: InAppUpdateNotification.station.notification(), object: nil, userInfo: ["id" : channel])
+        }).disposed(by: disposeBag)
 	}
 	
 	func makeTrack(id: Int, action: TrackAction) {
-		
+        let type: TrackUpdateRequest
+        switch action {
+        case .listen:
+            type = .listen
+        case .like:
+            type = .like(count: 1)
+        case .unlike:
+            type = .like(count: -1)
+        case .report(let msg):
+            type = .report(msg: msg)
+        }
+        RequestManager.shared.trackUpdate(id: id, type: type).subscribe(onNext: { (track) in
+            NotificationCenter.default.post(name: InAppUpdateNotification.track.notification(), object: nil, userInfo: ["id": track])
+        }).disposed(by: disposeBag)
 	}
 }
