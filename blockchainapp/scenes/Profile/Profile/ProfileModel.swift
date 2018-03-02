090af558ea15edeba866b9e8fcb955303faca036
Polina <abrosimowa@maugry.ru>
2018-03-02 15:31:33 +0500

    Save image in file manager

--- blockchainapp/scenes/Profile/Profile/ProfileModel.swift
+++ blockchainapp/scenes/Profile/Profile/ProfileModel.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
 
 protocol ProfileModelProtocol: class, ModelProtocol {
     func change(image: Data)
@@ -26,18 +28,64 @@ class ProfileModel: ProfileModelProtocol {
 
     weak var delegate: ProfileModelDelegate?
     
+    var disposeBag = DisposeBag()
+    
+    private let imageName = "profileImage"
+    
     init()
     {
         InAppUpdateManager.shared.subscribe(self)
     }
     
     func getData() {
-        delegate?.reload(name: UserSettings.name, image: UserSettings.image, language: UserSettings.language)
+        delegate?.reload(name: UserSettings.name, image: self.getImage(), language: UserSettings.language)
+    }
+    
+    func getImage() -> Data
+    {
+        var image = UIImage()
+        
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let filePath = documentsURL.appendingPathComponent("\(imageName).png").path
+        if FileManager.default.fileExists(atPath: filePath) {
+            image = UIImage(contentsOfFile: filePath)!
+        }
+        
+        if image.cgImage == nil
+        {
+            image = UIImage.init(named: "placeholder")!
+        }
+        
+        let data = UIImagePNGRepresentation(image)!
+        
+        return data
+    }
+    
+    func updateImage(image: Data) -> Observable<(Data)>
+    {
+        return Observable<(Data)>.create({ (observer) -> Disposable in
+            
+            do {
+                let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                let fileURL = documentsURL.appendingPathComponent("\(self.imageName).png")
+                try image.write(to: fileURL, options: .atomic)
+                observer.onCompleted()
+            } catch
+            {
+//                observer.onError(RequestError.invalidURL)
+            }
+            
+            return Disposables.create
+            {
+                print("Profile image saved")
+            }
+        })
     }
     
     func change(image: Data) {
-        UserSettings.image = image
-        self.delegate?.update(image: UserSettings.image)
+        self.updateImage(image: image).subscribe( onCompleted: {
+                self.delegate?.update(image: self.getImage())
+            }).disposed(by: disposeBag)
     }
     
     func change(name: String) {
