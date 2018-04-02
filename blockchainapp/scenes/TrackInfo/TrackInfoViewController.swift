1082afbb278251e395bf561c0ec63e76434d6946
Polina Abroimova <abrosimowa97@gmail.com>
2018-04-02 12:11:23 +0500

    refactor

--- blockchainapp/scenes/TrackInfo/TrackInfoViewController.swift
+++ blockchainapp/scenes/TrackInfo/TrackInfoViewController.swift
@@ -10,13 +10,8 @@ import UIKit
 
 class TrackInfoViewController: UIViewController {
     
-    // for comments
-//    var _tableView: UITableView = UITableView()
-    
     var trackInfoHeaderView: TrackInfoHeaderView!
     
-    var track: TrackViewModel!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,14 +33,6 @@ class TrackInfoViewController: UIViewController {
         trackInfoHeaderView.snp.makeConstraints({ (make) in
             make.edges.equalToSuperview()
         })
-        
-//        self.view.addSubview(_tableView)
-//        _tableView.snp.makeConstraints({ (make) in
-//            make.edges.equalToSuperview()
-//        })
-//
-//        _tableView.delegate = self
-//        _tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,28 +40,4 @@ class TrackInfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func update(_ track: TrackViewModel)
-    {
-        self.track = track
-    }
-
 }
-
-//extension TrackInfoViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 0
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        return UITableViewCell()
-//    }
-//
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        return TrackInfoHeaderView()
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-//        return 1500
-//    }
-//}
-
