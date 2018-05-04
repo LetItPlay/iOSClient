20ffbf7ffaa88722d5d1968a2d55047099444b77
Polina Abroimova <abrosimowa97@gmail.com>
2018-05-04 15:46:06 +0500

    working with requests for categories

--- blockchainapp/models/Channels/ChannelCategoryViewModel.swift
+++ blockchainapp/models/Channels/ChannelCategoryViewModel.swift
@@ -11,18 +11,10 @@ import Foundation
 class ChannelCategoryViewModel {
     var name: String = ""
     var channels: [CategoryChannelViewModel]!
-    var hideSeeAllButton: Bool = true
-    
-    init(name: String, channels: [CategoryChannelViewModel]) {
-        self.name = name
-        self.channels = channels.count > 5 ? Array(channels[0...4]) : channels
-        self.hideSeeAllButton = channels.count > 5 ? false : true
-    }
     
     init(category: ChannelCategory) {
         self.name = category.name
-        let channels = category.channels.count > 5 ? Array(category.channels[0...4]) : category.channels
+        let channels = category.channels
         self.channels = channels.map({CategoryChannelViewModel(channel: $0)})
-        self.hideSeeAllButton = category.channels.count > 5 ? false : true
     }
 }
